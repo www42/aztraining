79881fb021e8d4c1d7b89fd72122ed9113c2465b
www42 <thomas.jaekel@gfn.de>
2019-04-17 11:53:31 +0200

    point-to-site vpn created

---
+++ Point-to-Site_VPN/Create-Cert4VPN.ps1
@@ -0,0 +1,33 @@
+﻿# Create Certificate for Point-To-Site VPN
+# ----------------------------------------
+
+$rootCert = New-SelfSignedCertificate `
+              -Type Custom `
+              -KeySpec Signature `
+              -Subject 'CN=AdatumRootCertificate' `
+              -KeyExportPolicy Exportable `
+              -HashAlgorithm sha256 `
+              -KeyLength 2048 `
+              -CertStoreLocation 'Cert:\CurrentUser\My' `
+              -KeyUsageProperty Sign `
+              -KeyUsage CertSign `
+              -FriendlyName 'AdatumRootCertificate'
+
+[System.Convert]::ToBase64String($rootCert.RawData) | clip
+
+# paste it into browser "Public certificate data"
+
+
+New-SelfSignedCertificate `
+  -Type Custom `
+  -KeySpec Signature `
+  -Subject 'CN=AdatumClientCertificate' `
+  -KeyExportPolicy Exportable `
+  -HashAlgorithm sha256 `
+  -KeyLength 2048 `
+  -CertStoreLocation 'Cert:\CurrentUser\My' `
+  -Signer $rootCert `
+  -TextExtension @("2.5.29.37={text}1.3.6.1.5.5.7.3.2") `
+  -FriendlyName 'AdatumClientCertificate'
+
+Get-ChildItem Cert:\CurrentUser\My
