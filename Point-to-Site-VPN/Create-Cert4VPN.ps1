9a96357d0f20dfcaec2697952e4cd730110b08bb
www42 <thomas.jaekel@gfn.de>
2020-04-30 17:45:18 +0200

    ps1

--- Point-to-Site-VPN/Create-Cert4VPN.ps1
+++ Point-to-Site-VPN/Create-Cert4VPN.ps1
@@ -17,23 +17,54 @@ $rootCert = New-SelfSignedCertificate `
 
 # paste it into browser "Public certificate data"
 
+# = = = Alternative: Azure CLI =  = = = = = = = = = = = = = = = = = = = = = = =
+#                                                                             #
+$RootCertPublicData=[System.Convert]::ToBase64String($rootCert.RawData)
+$RootCertName="AdatumRoot"
+$GatewayName="AdatumGateway"
+$Hub_Group="az1000401-RG"
+$ProcessorArchitecture = "Amd64"
 
-New-SelfSignedCertificate `
-  -Type Custom `
-  -KeySpec Signature `
-  -Subject 'CN=AdatumClientCertificate' `
-  -KeyExportPolicy Exportable `
-  -HashAlgorithm sha256 `
-  -KeyLength 2048 `
-  -CertStoreLocation 'Cert:\CurrentUser\My' `
-  -Signer $rootCert `
-  -TextExtension @("2.5.29.37={text}1.3.6.1.5.5.7.3.2") `
-  -FriendlyName 'AdatumClientCertificate'
+az network vnet-gateway root-cert create --gateway-name $GatewayName --name $RootCertName --resource-group $Hub_Group --public-cert-data $RootCertPublicData
+#                                                                             #
+# = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =
 
-Get-ChildItem Cert:\CurrentUser\My
 
-# Cleanup - to do
-Remove-Item Cert:\CurrentUser\my\A90B8A57C2A61AB3591E0860C65637199166FE81
-Remove-Item Cert:\CurrentUser\my\90C7E3425208073D2725AD984695883435942571
+$ClientCert = New-SelfSignedCertificate `
+-Type Custom `
+-KeySpec Signature `
+-Subject 'CN=AdatumClientCertificate' `
+-KeyExportPolicy Exportable `
+-HashAlgorithm sha256 `
+-KeyLength 2048 `
+-CertStoreLocation 'Cert:\CurrentUser\My' `
+-Signer $rootCert `
+-TextExtension @("2.5.29.37={text}1.3.6.1.5.5.7.3.2") `
+-FriendlyName 'AdatumClientCertificate'
+
+Get-ChildItem Cert:\CurrentUser\My | where Subject -like "CN=Adatum*" | ft Subject, Issuer, Thumbprint
+
+az network vnet-gateway vpn-client generate --resource-group $Hub_Group --name $GatewayName --processor-architecture $ProcessorArchitecture
+
+# Why  '-o tsv' ? Because double quotes are not allowed in Invoke-RestMethod -Uri  
+$Uri = az network vnet-gateway vpn-client show-url --resource-group $Hub_Group --name $GatewayName -o tsv
+
+Invoke-RestMethod -Uri $Uri -OutFile c:\temp\VpNClient.zip
+Expand-Archive -Path c:\temp\VpnClient.zip -DestinationPath c:\temp\VpnClient
+& C:\temp\VpnClient\WindowsAmd64\VpnClientSetupAmd64.exe
+& ncpa.cpl
+
+Test-Netconnection 10.104.0.4
+Test-Netconnection 10.104.1.4
+Test-Netconnection 10.204.0.4
+Test-Netconnection 10.204.0.4 -TraceRoute
+
+
+dir c:\temp
+
+
+# Cleanup
+Remove-Item $ClientCert.PSPath
+Remove-Item $RootCert.PSPath
 
 # VPN entfernen und deinstallieren
